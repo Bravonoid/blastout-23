3ff4468969c6bc877c11caa11f118d80d8b0195a
Noth3r <azzakhrufaryannurihsan@gmail.com>
2022-12-01 10:42:33 +0700

    add(beranda/medpart): add desktop & mobile layout

---
+++ components/Beranda/Medpart.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Medpart() {
+  return (
+    <section className="max-w-8xl pt-12 mx-auto">
+      <h1 className="text-5xl font-bold text-center mb-12 sm:px-0 px-12">
+        MEDIA PARTNER
+      </h1>
+      <div className="flex items-center justify-evenly gap-y-7 sm:gap-3 flex-wrap">
+        {[...Array(10)].map((_, i) => (
+          <div
+            key={i}
+            className="bg-gray-500 h-20 basis-[40%] sm:basis-[24%] sm:h-44"
+          ></div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Medpart;
