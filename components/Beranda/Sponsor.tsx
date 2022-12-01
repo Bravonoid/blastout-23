fa2a4a212e5538120f8e889a38fa6b9e2357d5d5
Noth3r <azzakhrufaryannurihsan@gmail.com>
2022-12-01 10:38:13 +0700

    add(beranda/sponsor): mobile layout

--- components/Beranda/Sponsor.tsx
+++ components/Beranda/Sponsor.tsx
@@ -4,9 +4,12 @@ function Sponsor() {
   return (
     <section className="max-w-8xl pt-12 mx-auto">
       <h1 className="text-5xl font-bold text-center mb-12">Sponsor</h1>
-      <div className="flex items-center justify-evenly gap-3 flex-wrap">
+      <div className="flex items-center justify-evenly gap-y-7 sm:gap-3 flex-wrap">
         {[...Array(10)].map((_, i) => (
-          <div key={i} className="bg-gray-500 basis-[24%] h-44"></div>
+          <div
+            key={i}
+            className="bg-gray-500 h-20 basis-[40%] sm:basis-[24%] sm:h-44"
+          ></div>
         ))}
       </div>
     </section>
