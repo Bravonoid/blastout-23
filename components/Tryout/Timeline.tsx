0fda4bb66a15c26ea258c114c13c403fc4567d41
Noth3r <azzakhrufaryannurihsan@gmail.com>
2022-12-19 20:33:15 +0700

    add(tryout/timeline): add timeline styling

--- components/Tryout/Timeline.tsx
+++ components/Tryout/Timeline.tsx
@@ -1,3 +1,5 @@
+import styles from "./Timeline.module.css";
+
 function Timeline() {
   const data = [
     {
@@ -15,22 +17,67 @@ function Timeline() {
       date: "9 Januari 2022",
       desc: "Lorem ipsum",
     },
+    {
+      id: 4,
+      date: "10 Januari 2022",
+      desc: "Lorem ipsum",
+    },
+    {
+      id: 5,
+      date: "11 Januari 2022",
+      desc: "Lorem ipsum",
+    },
   ];
 
   return (
-    <section className="max-w-8xl mx-auto py-14">
-      <h1 className="text-center text-4xl font-bold mb-8">TIMELINE</h1>
-      <div className="w-full flex justify-between items-center">
-        {data.map(({ id, date, desc }) => (
-          <div
-            key={id}
-            className="flex items-center font-semibold flex-col space-y-2"
-          >
-            <h1 className="text-xl">{date}</h1>
-            <div className="w-10 h-10 rounded-full border-2"></div>
-            <p className="text-2xl">{desc}</p>
-          </div>
-        ))}
+    <section className="bg-[#0F0B36]">
+      <div className="max-w-8xl h-screen mx-auto flex flex-col justify-center items-center text-[#F0F0F0]">
+        <h1
+          style={{
+            textShadow:
+              "4px 0px 8px rgba(185, 35, 69, 0.5), -4px 0px 8px rgba(199, 126, 19, 0.4)",
+          }}
+          className="text-center text-6xl font-bold mb-20"
+        >
+          TIMELINE
+        </h1>
+        <div className={styles.stepperWrapper}>
+          {data.map(({ id, date, desc }) => {
+            const completed = id < 3;
+            const active = id == 3;
+
+            return (
+              <div
+                key={id}
+                className={`${styles.stepperItem} ${
+                  active ? styles.stepperItemActive : ""
+                } ${completed ? styles.stepperItemCompleted : ""}`}
+              >
+                <div
+                  className={`text-3xl font-bold mb-5 ${
+                    active || completed ? "text-[#ff0070]" : "text-[#d9d9d9]"
+                  }`}
+                >
+                  {date}
+                </div>
+                <button
+                  className={`${styles.stepCounter} ${
+                    active ? styles.stepCounterActive : ""
+                  } ${completed ? styles.stepCounterCompleted : ""}`}
+                >
+                  <p className="text-4xl font-bold">{id}</p>
+                </button>
+                <div
+                  className={`text-4xl font-bold ${
+                    active || completed ? "text-[#8438ED]" : "text-[#d9d9d9]"
+                  }`}
+                >
+                  {desc}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
