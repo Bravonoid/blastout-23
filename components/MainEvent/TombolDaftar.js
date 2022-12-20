fa24da37e2cf2b451df77f6238c507f8b2115ff0
eRBeGe <adryanpaw853@gmail.com>
2022-12-20 11:43:12 +0700

    Bang Diki, Alhamdulillah Footer dah jadi

--- components/MainEvent/TombolDaftar.js
+++ components/MainEvent/TombolDaftar.js
@@ -2,9 +2,9 @@ import React from "react";
 const TombolDaftar = () => {
   return (
     <>
-      <div class="flex justify-center">
-        <div class="my-10">
-          <a href="#" class="px-5 py-3 font-bold bg-[#D9D9D9]">
+      <div className="flex justify-center">
+        <div className="my-10">
+          <a href="#" className="px-5 py-3 font-bold bg-[#D9D9D9]">
             DAFTAR
           </a>
         </div>
--- pages/MainEvent.js
+++ pages/MainEvent.js
@@ -2,14 +2,16 @@ import React from "react";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import TombolDaftar from "../components/MainEvent/TombolDaftar";
+import Footer from "../components/Footer";
 const MainEvent = () => {
   return (
     <>
       <Head>
         <title>MainEvent . Blastout 2023</title>
       </Head>
-      <Navbar/>
+      <Navbar />
       <TombolDaftar />
+      <Footer/>
     </>
   );
 };
