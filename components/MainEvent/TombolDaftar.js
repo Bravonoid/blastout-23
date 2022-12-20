cdb01a64c9fe24fd5f6cc0c5f544b1a99a91b043
eRBeGe <adryanpaw853@gmail.com>
2022-12-20 09:48:22 +0700

    Tak tambahin TombolDaftar buat bang Diki

---
+++ components/MainEvent/TombolDaftar.js
@@ -0,0 +1,14 @@
+import React from "react";
+const TombolDaftar = () => {
+  return (
+    <>
+        <div class="flex justify-center">
+            <div class="my-10">
+                <a href="#" class="px-5 py-3 font-bold bg-[#D9D9D9]">DAFTAR</a>
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default TombolDaftar;
---
+++ pages/MainEvent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+import TombolDaftar from "../components/MainEvent/TombolDaftar";
+const MainEvent = () => {
+  return (
+    <>
+      <Head>
+        <title>MainEvent . Blastout 2023</title>
+      </Head>
+      <TombolDaftar/>
+    </>
+  );
+};
+export default MainEvent;
