8eea5f1753687c852831aaf08e4ffc1144ca46f6
eRBeGe <adryanpaw853@gmail.com>
2022-12-26 18:40:25 +0700

    bismillah tombol daftar mobile masuk bang

--- components/Footer.js
+++ components/Footer.js
@@ -4,35 +4,43 @@ const Footer = () => {
     <>
       <div className="flex justify-center my-20 w-[100vw]">
         <div className="w-[100vw]">
-            <img src="Footer.svg" alt="" className="w-[100vw]"></img>
+          <img src="Footer.svg" alt="" className="w-[100vw]"></img>
         </div>
         <div className="mt-[40px] absolute flex z-[10]">
-            <img src="BLASTOUT 2023.png" alt="" className="h-[20px]"></img>
+          <img src="BLASTOUT 2023.png" alt="" className="h-[20px]"></img>
         </div>
-        <div className= "mt-[80px] absolute flex z-[10]">
-            <div className="mx-4 flex">
-                <a href="#" className="text-[#FBDF97] text-lg font-inter font-bold">BERANDA</a>
-            </div>
-            <div className="mx-4 flex">
-                <a href="#" className="text-[#FBDF97] text-lg font-inter">TENTANG</a>
-            </div>
-            <div className="mx-4 flex">
-                <a href="#" className="text-[#FBDF97] text-lg font-inter">TRYOUT</a>
-            </div>
-            <div className="mx-4 flex">
-                <a href="#" className="text-[#FBDF97] text-lg font-inter">MAIN EVENT</a>
-            </div>
+        <div className="mt-[80px] absolute flex z-[10]">
+          <div className="mx-4 flex">
+            <a href="#" className="text-[#FBDF97] text-lg font-inter font-bold">
+              BERANDA
+            </a>
+          </div>
+          <div className="mx-4 flex">
+            <a href="#" className="text-[#FBDF97] text-lg font-inter">
+              TENTANG
+            </a>
+          </div>
+          <div className="mx-4 flex">
+            <a href="#" className="text-[#FBDF97] text-lg font-inter">
+              TRYOUT
+            </a>
+          </div>
+          <div className="mx-4 flex">
+            <a href="#" className="text-[#FBDF97] text-lg font-inter">
+              MAIN EVENT
+            </a>
+          </div>
         </div>
         <div className="absolute flex z-[10] justify-center mt-[130px]">
-            <img src="Group 11.svg" className="flex mx-2 w-[40px]"></img>
-            <img src="Group 12.svg" className="flex mx-2 w-[40px]"></img>
+          <img src="Group 11.svg" className="flex mx-2 w-[40px]"></img>
+          <img src="Group 12.svg" className="flex mx-2 w-[40px]"></img>
         </div>
         <div className="absolute flex z-[10] justify-center mt-[185px]">
-            <div className="font-inter text-[#FBDF97] font-semibold text-lg">
-                IT BLASTOUT 2023
-            </div>
+          <div className="font-inter text-[#FBDF97] font-semibold text-lg">
+            IT BLASTOUT 2023
+          </div>
         </div>
-    </div>
+      </div>
     </>
   );
 };
--- components/MainEvent/TombolDaftar.js
+++ components/MainEvent/TombolDaftar.js
@@ -2,11 +2,11 @@ import React from "react";
 const TombolDaftar = () => {
   return (
     <>
-      <div className="container flex mx-auto">
+      <div className="flex mx-auto">
         <div className="my-20 mx-auto">
           <a
             href="#"
-            className="inline-block px-3 py-2 font-bold bg-gradient-to-l to-pink-600 from-purple-600 text-[#240724] uppercase tracking-wider text-2xl hover:text-white hover:shadow-pink-300 hover:shadow-lg"
+            className="px-3 py-2 text-lg font-bold bg-gradient-to-l to-pink-600 from-purple-600 text-[#240724] lg:text-2xl hover:text-white hover:shadow-pink-300 hover:shadow-lg"
           >
             DAFTAR
           </a>
