4e1be47bbac230f7e6eb5bc6b47b30817ee9227c
Andyan Yoga <65155073+zenxiety@users.noreply.github.com>
2022-12-29 20:52:02 +0700

    overwritten files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter, Poppins, Montserrat } from "@next/font/google";
+import { useEffect } from "react";
+import 'locomotive-scroll/dist/locomotive-scroll.css'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,11 +21,16 @@ const montserrat = Montserrat({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    window.dispatchEvent(new Event('resize'))
+  }, [Component])
+
   return (
     <main
       className={`${inter.variable} ${poppins.variable} ${montserrat.variable}`}
     >
       <Component {...pageProps} />
     </main>
+
   );
 }
