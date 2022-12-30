96029fdc288202a85b75223957eb29fe5e7ec4b2
eRBeGe <adryanpaw853@gmail.com>
2022-12-30 21:27:43 +0700

    revisi navbar n footer dekstop

--- components/ComingSoon.js
+++ components/ComingSoon.js
@@ -1,31 +1,32 @@
 import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 const ComingSoon = () => {
   return (
     <>
       <div className="visible lg:invisible">
         <div className="bg-[#0A071F] w-[100vw] h-[700px] flex justify-center items-center">
-        <div className="absolute flex z-[10] text-6xl mb-[340px] text-black font-extrabold font-outline-4 font-inter opacity-20">
+          <div className="absolute flex z-[10] text-6xl mb-[340px] text-black font-extrabold font-outline-4 font-inter opacity-20">
             COMING
           </div>
-        <div className="absolute flex z-[10] text-6xl mb-[320px] text-black font-extrabold font-outline-4 font-inter opacity-30">
+          <div className="absolute flex z-[10] text-6xl mb-[320px] text-black font-extrabold font-outline-4 font-inter opacity-30">
             COMING
           </div>
-        <div className="absolute flex z-[10] text-6xl mb-[295px] text-black font-extrabold font-outline-4 font-inter opacity-40">
+          <div className="absolute flex z-[10] text-6xl mb-[295px] text-black font-extrabold font-outline-4 font-inter opacity-40">
             COMING
           </div>
-        <div className="absolute flex z-[10] text-6xl mb-[265px] text-black font-extrabold font-outline-4 font-inter opacity-50">
+          <div className="absolute flex z-[10] text-6xl mb-[265px] text-black font-extrabold font-outline-4 font-inter opacity-50">
             COMING
           </div>
-        <div className="absolute flex z-[10] text-6xl mb-[230px] text-black font-extrabold font-outline-4 font-inter opacity-60">
+          <div className="absolute flex z-[10] text-6xl mb-[230px] text-black font-extrabold font-outline-4 font-inter opacity-60">
             COMING
           </div>
-        <div className="absolute flex z-[10] text-6xl mb-[190px] text-black font-extrabold font-outline-4 font-inter opacity-70">
+          <div className="absolute flex z-[10] text-6xl mb-[190px] text-black font-extrabold font-outline-4 font-inter opacity-70">
             COMING
           </div>
-        <div className="absolute flex z-[10] text-6xl mb-[145px] text-black font-extrabold font-outline-4 font-inter opacity-80">
+          <div className="absolute flex z-[10] text-6xl mb-[145px] text-black font-extrabold font-outline-4 font-inter opacity-80">
             COMING
           </div>
-        <div className="absolute flex z-[10] text-6xl mb-[100px] text-black font-extrabold font-outline-4 font-inter opacity-90">
+          <div className="absolute flex z-[10] text-6xl mb-[100px] text-black font-extrabold font-outline-4 font-inter opacity-90">
             COMING
           </div>
           <div className="absolute flex z-[10] text-6xl mb-[50px] text-black font-extrabold font-outline-4 font-inter">
@@ -37,95 +38,131 @@ const ComingSoon = () => {
           <div className="absolute flex z-[10] text-6xl mt-[340px] text-black font-extrabold font-outline-4 font-inter opacity-20">
             SOON
           </div>
-        <div className="absolute flex z-[10] text-6xl mt-[320px] text-black font-extrabold font-outline-4 font-inter opacity-30">
-        SOON
+          <div className="absolute flex z-[10] text-6xl mt-[320px] text-black font-extrabold font-outline-4 font-inter opacity-30">
+            SOON
           </div>
-        <div className="absolute flex z-[10] text-6xl mt-[295px] text-black font-extrabold font-outline-4 font-inter opacity-40">
-        SOON
+          <div className="absolute flex z-[10] text-6xl mt-[295px] text-black font-extrabold font-outline-4 font-inter opacity-40">
+            SOON
           </div>
-        <div className="absolute flex z-[10] text-6xl mt-[265px] text-black font-extrabold font-outline-4 font-inter opacity-50">
-        SOON
+          <div className="absolute flex z-[10] text-6xl mt-[265px] text-black font-extrabold font-outline-4 font-inter opacity-50">
+            SOON
           </div>
-        <div className="absolute flex z-[10] text-6xl mt-[230px] text-black font-extrabold font-outline-4 font-inter opacity-60">
-        SOON
+          <div className="absolute flex z-[10] text-6xl mt-[230px] text-black font-extrabold font-outline-4 font-inter opacity-60">
+            SOON
           </div>
-        <div className="absolute flex z-[10] text-6xl mt-[190px] text-black font-extrabold font-outline-4 font-inter opacity-70">
-        SOON
+          <div className="absolute flex z-[10] text-6xl mt-[190px] text-black font-extrabold font-outline-4 font-inter opacity-70">
+            SOON
           </div>
-        <div className="absolute flex z-[10] text-6xl mt-[145px] text-black font-extrabold font-outline-4 font-inter opacity-80">
-        SOON
+          <div className="absolute flex z-[10] text-6xl mt-[145px] text-black font-extrabold font-outline-4 font-inter opacity-80">
+            SOON
           </div>
-        <div className="absolute flex z-[10] text-6xl mt-[100px] text-black font-extrabold font-outline-4 font-inter opacity-90">
-        SOON
+          <div className="absolute flex z-[10] text-6xl mt-[100px] text-black font-extrabold font-outline-4 font-inter opacity-90">
+            SOON
           </div>
         </div>
       </div>
 
       <div className="invisible lg:visible flex justify-center items-center w-[100vw]">
-      <div>
-          <img src="./Coming Soon bg.svg" className=""></img>
-        </div>
-        <div className="absolute hidden lg:block z-[10] mb-[600px] h-[4px] w-[600px] bg-[#FFBD0C]">
-        </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mb-[520px] opacity-10">
+          <div>
+              <img src="./Coming Soon bg.svg" className="w-[100vw]"></img>
+          </div>
+        <Parallax pages={5}>
+
+        <ParallaxLayer 
+          speed={1}
+          offset={0}
+          factor={1}
+          sticky={{end:1}}
+          >
+          <div className="absolute right-[385px] top-[720px] z-[10] h-[4px] w-[600px] bg-[#FFBD0C]">
+          </div>
+          </ParallaxLayer>
+
+
+          <ParallaxLayer 
+          speed={1}
+          offset={0}
+          factor={0.1}
+          sticky={{start:0,end:1.5}}
+          >
+          </ParallaxLayer>
+
+          <ParallaxLayer 
+          offset={0} 
+          speed={1} 
+          factor={2}
+          sticky={{start:1,end:1.5}}
+          >
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[420px] opacity-10">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mb-[460px] opacity-20">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[440px] opacity-20">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mb-[400px] opacity-30">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[460px] opacity-30">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mb-[340px] opacity-40">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[480px] opacity-40">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mb-[280px] opacity-50">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[500px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mb-[220px] opacity-50">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[520px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mb-[160px] opacity-50">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[540px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mb-[100px] opacity-50">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[560px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mb-[40px] opacity-50">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[580px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[15] text-[70px] text-black font-extrabold font-outline-4 font-inter">
+        <div className="absolute right-[425px] top-[600px] z-[15] text-[70px] text-black font-extrabold font-outline-4 font-inter">
+              COMING SOON
+            </div>
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[620px] opacity-10">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mt-[520px] opacity-10">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[640px] opacity-20">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mt-[460px] opacity-20">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[660px] opacity-30">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mt-[400px] opacity-30">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[680px] opacity-40">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mt-[340px] opacity-40">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[700px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mt-[280px] opacity-50">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[720px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mt-[220px] opacity-50">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[740px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mt-[160px] opacity-50">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[760px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mt-[100px] opacity-50">
+        <div className="absolute z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter right-[425px] top-[780px] opacity-50">
           COMING SOON
         </div>
-        <div className="absolute flex z-[10] text-[70px] text-black font-extrabold font-outline-1 font-inter mt-[40px] opacity-50">
-          COMING SOON
-        </div>
-        <div className="absolute hidden lg:block z-[10] mt-[600px] h-[4px] w-[600px] bg-[#FFBD0C]">
-        </div>
+          </ParallaxLayer>
+
+          <ParallaxLayer 
+          speed={2}
+          offset={1.5}
+          factor={0.1}
+          sticky={{start:0,end:1.5}}
+          >
+          <div className="absolute right-[385px] top-[580px] z-[10] h-[4px] w-[600px] bg-[#FFBD0C]">
+          </div>
+          </ParallaxLayer>
+
+        </Parallax>
       </div>
     </>
   );
--- components/Footer.js
+++ components/Footer.js
@@ -2,15 +2,15 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="flex lg:justify-center my-[500px] w-[100vw]">
-        <div className="bg-[#0A0823] visible lg:invisible w-[100vw] h-[400px]">
+        <div className="bg-[#0A0823] lg:justify-center lg:items-center w-[100vw] lg:h-[280px] h-[400px] flex relative my-[200px]">
+          <div className="w-[100vw] bottom-0 absolute invisible lg:visible">
           <img
-            src="Footer.svg"
+            src="./Group 5908.svg"
             alt=""
-            className="w-[100vw] invisible lg:visible"
+            className=""
           ></img>
-        </div>
-        <div className="lg:mt-[40px] absolute lg:flex z-[10] lg:justify-center">
+          </div>
+        <div className="lg:top-[40px] absolute lg:flex z-[10] lg:justify-center">
           <div className="flex absolute w-[100vw]">
             <img
               src="BLASTOUT 2023.png"
@@ -24,60 +24,76 @@ const Footer = () => {
             className="visible lg:invisible mt-16 ml-10"
           ></img>
         </div>
-        <div className="lg:mt-[80px] absolute lg:flex z-[10] mt-[120px] ml-[25px]">
-          <div className="mx-4 my-2 lg:flex">
+        <div className="lg:top-[80px] lg:mt-0 absolute lg:flex z-[10] mt-[120px] ml-[25px]">
+          <div className="mx-[18px] my-2 lg:flex">
             <a
               href="#"
-              className="text-[#FBDF97] text-sm lg:text-lg font-inter font-bold"
+              className="text-[#FBDF97] text-sm lg:text-lg font-inter hover:font-bold"
             >
               BERANDA
             </a>
           </div>
-          <div className="mx-4 my-2 lg:flex">
+          <div className="mx-[18px] my-2 lg:flex">
             <a
               href="#"
-              className="text-[#FBDF97] text-sm lg:text-lg font-inter"
+              className="text-[#FBDF97] text-sm lg:text-lg font-inter hover:font-bold"
             >
               TENTANG
             </a>
           </div>
-          <div className="mx-4 my-2 lg:flex">
+          <div className="mx-[18px] my-2 lg:flex">
             <a
               href="#"
-              className="text-[#FBDF97] text-sm lg:text-lg font-inter"
+              className="text-[#FBDF97] text-sm lg:text-lg font-inter hover:font-bold"
             >
               TRYOUT
             </a>
           </div>
-          <div className="mx-4 my-2 lg:flex">
+          <div className="mx-[18px] my-2 lg:flex">
             <a
               href="#"
-              className="text-[#FBDF97] text-sm lg:text-lg font-inter"
+              className="text-[#FBDF97] text-sm lg:text-lg font-inter hover:font-bold"
             >
               MAIN EVENT
             </a>
           </div>
         </div>
-        <div className="absolute flex z-[10] justify-center lg:mt-[130px] mt-[260px] ml-8 lg:ml-0">
+        <div className="absolute flex z-[10] justify-center lg:mt-0 mt-[260px] ml-8 lg:ml-0 lg:top-[140px]">
+          <div className="flex mx-2 lg:mx-[10px] lg:w-[45px] w-[30px] group">
           <a href="#" className="">
+          <img
+              src="./Group 11.svg"
+              className="group-hover:hidden block"
+            >
+            </img>
             <img
-              src="Group 11.svg"
-              className="flex mx-2 lg:w-[40px] w-[30px]"
-            ></img>
+              src="./Group 11h.svg"
+              className="group-hover:block hidden"
+            >
+            </img>
           </a>
+          </div>
+          <div className="flex mx-2 lg:mx-[10px] lg:w-[45px] w-[30px] group">
           <a href="#" className="">
+          <img
+              src="./Group 12.svg"
+              className="group-hover:hidden block"
+            >
+            </img>
             <img
-              src="Group 12.svg"
-              className="flex mx-2 lg:w-[40px] w-[30px]"
-            ></img>
-          </a>
+              src="./Group 12h.svg"
+              className="group-hover:block hidden"
+            >
+            </img>
+            </a>
+          </div>
         </div>
-        <div className="absolute lg:flex z-[10] justify-center mt-[310px] ml-10 lg:mt-[185px] lg:ml-0">
+        <div className="absolute lg:flex z-[10] justify-center mt-[310px] ml-10 lg:mt-0 lg:ml-0 lg:top-[210px]">
           <div className="font-inter text-[#FBDF97] font-semibold text-xs lg:text-lg">
             IT BLASTOUT 2023
           </div>
         </div>
-      </div>
+        </div>
     </>
   );
 };
