0e06da653ae4e8dc68842c48999faa037bd7794b
farrelamroe@gmail.com <88188954+farrelamroe2003@users.noreply.github.com>
2022-12-30 13:08:52 +0700

    slicing

--- components/Beranda/Sekilas.js
+++ components/Beranda/Sekilas.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Line from "../../assets/Beranda/Group 16.png";
+import Line from "../../assets/Beranda/Vector 2.png";
 import { useRouter } from "next/router";
 const Sekilas = () => {
   const router = useRouter();
@@ -13,7 +13,7 @@ const Sekilas = () => {
             BLASTOUT 
             <Link href="./Tentang"><span class="hover:shadow-[0px_0px_9px_9px_#FFBD0C] duration-300 text-[#FFBD0C] p-0">&#x3e;</span></Link>
           </h1>
-          <Image src={Line} alt="line" class="block mx-auto pt-5" />
+          <Image src={Line} alt="line" class="block mx-auto pt-5 w-[300px]" />
           <p class="text-center px-20 font-Poppins text-[36px]">
           BlastOut merupakan program kerja dari Balairung Klass, organisasi mahasiswa daerah dengan anggota mahasiswa UGM yang berdomisili di Klaten. BlastOut terdiri dari rangkaian acara briefing ke SMA di Kab. Klaten, tryout beserta pembahasan soal, talkshow, expo kampus, dan penampilan hiburan.
           </p>
