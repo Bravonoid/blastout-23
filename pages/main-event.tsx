5f020e4f42ebb0e4f549bace3772d12c60434691
LatianOTI <ccobaabocc00@gmail.com>
2023-01-01 07:02:42 +0700

    gabungin main event page

--- pages/main-event.tsx
+++ pages/main-event.tsx
@@ -1,15 +1,21 @@
-import Timeline from "../components/Beranda/Timeline";
-import Hero from "../components/mainevent/Hero";
-import Lokasi from "../components/mainevent/Lokasi";
-import WhatsInside from "../components/mainevent/WhatsInside";
+// import Timeline from "../components/Beranda/Timeline";
+import Hero from "../components/MainEvent/Hero";
+import Lokasi from "../components/MainEvent/Lokasi";
+import WhatsInside from "../components/MainEvent/WhatsInside";
+import Navbar from "../components/Navbar";
+import TombolDaftar from "../components/MainEvent/TombolDaftar";
+import Footer from "../components/Footer";
 
 function mainEvent() {
   return (
     <div className="bg-blastoutdarkpurple overflow-hidden">
+      <Navbar />
       <Hero />
       {/* <Timeline /> */}
       <WhatsInside />
       <Lokasi />
+      <TombolDaftar />
+      <Footer />
     </div>
   );
 }
