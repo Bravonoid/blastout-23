3fbc3f3649d8afc9b351e88d4fd440017bdd0f91
Arya Nur <azzakhrufaryannurihsan@gmail.com>
2023-01-01 11:38:17 +0700

    add(tryout/timeline): create dynamic date active

--- components/Tryout/Timeline.tsx
+++ components/Tryout/Timeline.tsx
@@ -1,32 +1,36 @@
 import Image from "next/image";
 import styles from "./Timeline.module.css";
 
-function Timeline() {
+type Timeline = {
+  pageNumber: number;
+};
+
+function Timeline({ pageNumber }: Timeline) {
   const data = [
     {
       id: 1,
-      date: "7 Januari",
-      desc: "Lorem ipsum",
+      date: "3 - 20 Januari",
+      desc: "Briefing",
     },
     {
       id: 2,
-      date: "8 Januari",
-      desc: "Lorem ipsum",
+      date: "4 Februari",
+      desc: "Tryout",
     },
     {
       id: 3,
-      date: "9 Januari",
-      desc: "Lorem ipsum",
+      date: "5 Febuari",
+      desc: "Talk Show",
     },
     {
       id: 4,
-      date: "10 Januari",
-      desc: "Lorem ipsum",
+      date: "5 Februari",
+      desc: "Faculty Fair",
     },
     {
       id: 5,
-      date: "11 Januari",
-      desc: "Lorem ipsum",
+      date: "5 Februari",
+      desc: "Hiburan",
     },
   ];
 
@@ -130,8 +134,8 @@ function Timeline() {
         </h1>
         <div className={styles.stepperWrapper}>
           {data.map(({ id, date, desc }) => {
-            const completed = id < 3;
-            const active = id == 3;
+            const completed = id < pageNumber;
+            const active = id == pageNumber;
             const odd = id % 2 == 0;
 
             return (
