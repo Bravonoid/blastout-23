11c5e4865e7be1840b5778fd6212d567cb7096d0
Andyan Yoga <65155073+zenxiety@users.noreply.github.com>
2023-01-02 01:14:17 +0700

    add(beranda): mobile timeline

--- components/Beranda/Timeline.tsx
+++ components/Beranda/Timeline.tsx
@@ -2,47 +2,89 @@ import Image from "next/image";
 
 function Timeline() {
   return (
-    <div className="w-full py-32 relative bg-blastoutdarkpurple">
-      <h2 className="font-bold text-blastoutwhite text-glow text-2xl lg:text-5xl uppercase font-inter text-center mb-36 2xl:mb-48">
-        Timeline
-      </h2>
-      <div className="relative w-[85%] mx-auto group">
-        <div className="w-full relative z-10">
-          <Image
-            src="/beranda/timeline/arrow.svg"
-            alt="arrow bg"
-            width={1000}
-            height={1000}
-            className="w-full relative bg-blastoutdarkpurple"
-          />
-          <div className="w-full h-full grid grid-cols-5 grid-rows-2 absolute top-0 left-0 pl-[10vw] pr-[10vw]">
-            {Date("03-20 JAN", false)}
-            <div />
-            {Date("05 FEB", false)}
-            <div />
-            {Date("05 FEB", false)}
-            <div />
-            {Date("04 FEB", true)}
-            <div />
-            {Date("05 FEB", true)}
+    <>
+      <style jsx>{`
+      .underline-event::after {
+        content: '';
+        height: 4px;
+        width: 100%;
+        position: absolute;
+        bottom: -.4em;
+        left: 50%;
+        transform: translateX(-50%);
+        background-color: #FF0070;
+        border-radius: 99px;
+      }
+    `}</style>
+      <div className="w-full py-12 xl:py-24 2xl:py-36 relative bg-blastoutdarkpurple group">
+        <div className="font-bold text-blastoutwhite text-glow text-2xl sm:text-5xl uppercase font-inter text-center mb-[12vw] lg:mb-36 2xl:mb-60 relative w-fit mx-auto">
+          Timeline
+          <Image src="/beranda/timeline/underline.svg" alt="underline" width={1000} height={1000} className="absolute bottom-0 left-0 w-full -mb-1 sm:-mb-4" />
+        </div>
+        <div className="hidden lg:block relative w-[85%] mx-auto group my-24">
+          <div className="w-full relative z-10">
+            <Image
+              src="/beranda/timeline/arrow.svg"
+              alt="arrow bg"
+              width={1000}
+              height={1000}
+              className="w-full relative bg-blastoutdarkpurple"
+            />
+            <div className="w-full h-full grid lg:grid-cols-5 lg:grid-rows-2 absolute top-0 left-0 pl-[10vw] pr-[10vw]">
+              {Date("03-20 JAN", false)}
+              <div />
+              {Date("05 FEB", false)}
+              <div />
+              {Date("05 FEB", false)}
+              <div />
+              {Date("04 FEB", true)}
+              <div />
+              {Date("05 FEB", true)}
+            </div>
+          </div>
+          <div className="w-full h-[300%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pl-[10vw] pr-[10vw]">
+            <div className="w-full h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid grid-cols-5 grid-rows-3 pl-[10vw] pr-[10vw]">
+              {Event("Briefing", false)}
+              <div />
+              {Event("Blastalk", false)}
+              <div />
+              {Event("Entertainment", false)}
+              <div className="col-span-5" />
+              <div />
+              {Event("Tryout", true)}
+              <div />
+              {Event("Campus Fair", true)}
+            </div>
           </div>
         </div>
-        <div className="w-full h-[300%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pl-[10vw] pr-[10vw]">
-          <div className="w-full h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid grid-cols-5 grid-rows-3 pl-[10vw] pr-[10vw]">
-            {Event("Briefing", false)}
-            <div />
-            {Event("Blastalk", false)}
-            <div />
-            {Event("Entertainment", false)}
-            <div className="col-span-5" />
-            <div />
-            {Event("Tryout", true)}
-            <div />
-            {Event("Campus Fair", true)}
+        {/* MOBILE */}
+        <div className="grid grid-cols-2 -translate-x-[15%] sm:-translate-x-[12%] lg:hidden">
+          <div className="relative flex justify-center bg-blastoutdarkpurple z-10 transition duration-1000 translate-x-10 group-hover:translate-x-0">
+            <Image
+              src="/beranda/timeline/arrow-v.svg"
+              alt="arrow bg"
+              width={1000}
+              height={1000}
+              className="w-1/2 sm:w-1/3 bg-blastoutdarkpurple ml-auto"
+            />
+            <div className="w-1/2 sm:w-1/3 pt-[18vw] sm:pt-[12vw] pb-48 grid grid-rows-5 place-content-center text-center gap-y-[16vw] sm:gap-y-[10vw] absolute top-0 right-0">
+              {Date("03-20 JAN", false)}
+              {Date("04 FEB", false)}
+              {Date("05 FEB", false)}
+              {Date("05 FEB", false)}
+              {Date("05 FEB", false)}
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-[22.5vw] sm:gap-y-[14vw] pt-[20vw] sm:pt-[13vw] ml-[5vw] transition duration-1000 -translate-x-[150%] group-hover:translate-x-0">
+            <div className="font-inter font-black text-[6vw] sm:text-[4vw] text-white leading-none flex items-center w-fit h-fit uppercase tracking-wider underline-event relative whitespace-nowrap">Briefing</div>
+            <div className="font-inter font-black text-[6vw] sm:text-[4vw] text-white leading-none flex items-center w-fit h-fit uppercase tracking-wider underline-event relative whitespace-nowrap">Tryout</div>
+            <div className="font-inter font-black text-[6vw] sm:text-[4vw] text-white leading-none flex items-center w-fit h-fit uppercase tracking-wider underline-event relative whitespace-nowrap">Blastalk</div>
+            <div className="font-inter font-black text-[6vw] sm:text-[4vw] text-white leading-none flex items-center w-fit h-fit uppercase tracking-wider underline-event relative whitespace-nowrap">Campus Fair</div>
+            <div className="font-inter font-black text-[6vw] sm:text-[4vw] text-white leading-none flex items-center w-fit h-fit uppercase tracking-wider underline-event relative whitespace-nowrap">Entertainment</div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -51,16 +93,16 @@ export default Timeline;
 function Date(title, bottom) {
   return (
     <div
-      className={`relative font-inter font-black text-2xl xl:text-[1.75rem] 2xl:text-4xl text-blastoutdarkpurple w-full h-full flex leading-none ${
-        bottom && "items-end"
-      }`}
+      className={`relative font-inter font-black text-[6vw] sm:text-[4vw] lg:text-2xl xl:text-[1.75rem] 2xl:text-4xl text-blastoutdarkpurple w-full h-full flex justify-center lg:justify-start lg:text-start leading-none lg:my-0
+      ${bottom && "items-end"
+        }`}
     >
-      <span className="ml-2 2xl:ml-4">
+      <span className="lg:ml-[1vw] 2xl:ml-4">
         {title.split(" ")[0]}
         <br />
         {title.split(" ")[1]}
       </span>
-      <div className="absolute top-0 left-0 h-full w-[4px] 2xl:w-[6px] bg-blastoutdarkpurple" />
+      <div className="hidden lg:inline-block absolute top-0 left-0 h-full w-[4px] 2xl:w-[6px] bg-blastoutdarkpurple" />
     </div>
   );
 }
@@ -68,31 +110,33 @@ function Date(title, bottom) {
 function Event(title, bottom) {
   return (
     <div
-      className={`relative w-full h-full grid grid-rows-2 transition duration-1000 ${
-        !bottom
-          ? "translate-y-[80%] group-hover:translate-y-0"
-          : "-translate-y-[80%] group-hover:translate-y-0"
-      }`}
+      className={`relative w-full h-full lg:grid grid-rows-2 transition duration-1000 ${!bottom
+        ? "translate-y-[80%] group-hover:translate-y-0"
+        : "-translate-y-[80%] group-hover:translate-y-0"
+        }`}
     >
       <div
-        className={`relative w-full h-full ${
-          !bottom ? "row-start-2" : "row-start-1"
-        }`}
-      >
-        <div className="absolute top-0 left-0 h-full w-[4px] 2xl:w-[6px] bg-blastoutpink" />
-        <div
-          className={`absolute left-0 h-[4px] 2xl:h-[6px] w-full bg-blastoutpink ${
-            !bottom ? "top-0" : "bottom-0"
+        className={`relative w-full h-full ${!bottom ? "row-start-2" : "row-start-1"
           }`}
+      >
+        <div className="hidden lg:inline-block absolute top-0 left-0 h-full w-[4px] 2xl:w-[6px] bg-blastoutpink" />
+        <div
+          className={`hidden lg:inline-block absolute left-0 h-[4px] 2xl:h-[6px] w-full bg-blastoutpink ${!bottom ? "top-0" : "bottom-0"
+            }`}
         />
       </div>
       <div
-        className={`font-inter font-extrabold text-2xl 2xl:text-3xl 2xl:my-2 text-blastoutwhite uppercase flex justify-center whitespace-nowrap ${
-          !bottom ? "items-end row-start-1" : "row-start-2"
-        }`}
+        className={`font-inter font-extrabold text-sm lg:text-lg 2xl:text-3xl 2xl:my-2 text-blastoutwhite uppercase flex justify-center whitespace-nowrap ${!bottom ? "items-end row-start-1" : "row-start-2"
+          }`}
       >
         {title}
       </div>
     </div>
   );
+}
+
+function EventMobile(title) {
+  return (
+    <div className="font-inter font-black text-2xl text-white flex items-center w-fit mt-12 uppercase tracking-wider underline-event relative">{title}</div>
+  )
 }
