26aaed9596c576e99bd7822e9956da88cb9451b8
Diki Bagastama <dikibagas139@gmail.com>
2023-01-01 07:58:36 +0700

    fix: hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
 import Head from "next/head";
-import Image from "next/image";
-import Beranda from "./Beranda";
+import Header from "../components/Beranda/Header";
+import Navbar from "../components/Navbar";
+import Sekilas from "../components/Beranda/Sekilas";
+import Acara from "../components/Beranda/Acara";
+import Testimoni from "../components/Beranda/Testimoni";
+import Footer from "../components/Footer";
 export default function Home() {
   return (
     <>
-      <Beranda />
+      <Head>
+        <title>Beranda | Blastout 2023</title>
+      </Head>
+      <Navbar />
+      <Header />
+      <Sekilas />
+      <Acara />
+      <Testimoni />
+      <Footer />
     </>
   );
 }
