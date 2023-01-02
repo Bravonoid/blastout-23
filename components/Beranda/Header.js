e171f1328e48cb5a9c582d37fc96978ee151d5d2
Diki Bagastama <dikibagas139@gmail.com>
2023-01-02 11:37:38 +0700

    feat(beranda): removing subtitle hero

--- components/Beranda/Header.js
+++ components/Beranda/Header.js
@@ -7,14 +7,14 @@ const Header = () => {
   return (
     <>
       <div
-        class="h-screen overflow-hidden shadow-inner-bottom bg-blastoutdarkpurple bg-cover bg-center bg-cover flex justify-center items-center"
+        class="h-screen overflow-hidden shadow-inner-bottom bg-blastoutdarkpurple bg-center bg-cover flex justify-center items-center"
         style={{ backgroundImage: `url(${background1.src})` }}
       >
         <div class="flex flex-col justify-center items-center">
           <Image src={title} alt="title" />
-          <p className="bg-gradient-to-br from-[#8338ED] to-[#FF0070] px-3 py-2 rounded-full font-bold text-white text-sm lg:text-4xl lg:-mt-10 lg:px-6 lg:py-3 md:text-2xl">
+          {/* <p className="bg-gradient-to-br from-[#8338ED] to-[#FF0070] px-3 py-2 rounded-full font-bold text-white text-sm lg:text-4xl lg:-mt-10 lg:px-6 lg:py-3 md:text-2xl">
             Adaptasi, Susun Strategi, Raih Mimpi
-          </p>
+          </p> */}
         </div>
       </div>
     </>
