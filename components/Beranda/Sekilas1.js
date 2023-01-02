289a08373a52cfc439406c408ac168df284aa2bd
LatianOTI <ccobaabocc00@gmail.com>
2023-01-02 11:15:41 +0700

    gedein mobile sekilas

--- components/Beranda/Sekilas1.js
+++ components/Beranda/Sekilas1.js
@@ -11,17 +11,17 @@ const Sekilas1 = () => {
 
   return (
     <div className="bg-[#0F0B36] flex flex-col items-center justify-center text-white pt-20">
-      <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center w-full">
         <div
-          className="flex flex-row justify-evenly w-[80%] h-[6vw] sm:h-[4vw] items-center cursor-pointer"
+          className="flex flex-row justify-evenly w-[65%] sm:w-[30%] h-[8vw] sm:h-[4vw] items-center cursor-pointer"
           onClick={clickHandler}
         >
-          <div className="bg-[url('/beranda/BLASTOUT.png')] w-[70%] h-[6vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat"></div>
-          <div className="bg-[url('/beranda/arrow.png')] w-[10%] h-[4vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat hover:scale-[1.05]"></div>
+          <div className="bg-[url('/beranda/BLASTOUT.png')] w-[70%] sm:w-[70%] h-[6vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat"></div>
+          <div className="bg-[url('/beranda/arrow.png')] w-[10%] h-[5vw] sm:h-[2.5vw] bg-contain bg-center bg-no-repeat hover:scale-[1.05]"></div>
         </div>
-        <div className="bg-[url('/beranda/vector2.png')] w-[50vw] h-[1.5vw] sm:w-[32.5vw] sm:h-[1vw] bg-contain bg-center bg-no-repeat"></div>
+        <div className="bg-[url('/beranda/vector2.png')] w-[70vw] h-[2vw] scale-y-[0.5] sm:w-[32.5vw] sm:h-[1vw] bg-contain bg-center bg-no-repeat"></div>
       </div>
-      <div className="w-[60vw] font-inter text-[10px] sm:text-3xl text-center my-[5vh] sm:my-[4vw]">
+      <div className="w-[60vw] font-inter text-[lg] sm:text-3xl text-center mt-[2.5vh] mb-[5vh] sm:my-[4vw]">
         BlastOut merupakan program kerja dari Balairung Klass, organisasi
         mahasiswa daerah dengan anggota mahasiswa UGM yang berdomisili di
         Klaten. BlastOut terdiri dari rangkaian acara briefing ke SMA di Kab.
@@ -29,8 +29,8 @@ const Sekilas1 = () => {
         penampilan hiburan.
       </div>
       <div className="my-[2vw]">
-        <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[3vw] bg-contain bg-center bg-no-repeat"></div>
-        <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[3vw] bg-contain bg-center bg-no-repeat"></div>
+        <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1]"></div>
+        <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1]"></div>
       </div>
     </div>
   );
