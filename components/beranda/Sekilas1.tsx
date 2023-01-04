675dd651363cf0b6484415f97b89863962fd481f
LatianOTI <ccobaabocc00@gmail.com>
2023-01-04 11:42:03 +0700

    animasi blastout cmiiw

--- components/beranda/Sekilas1.tsx
+++ components/beranda/Sekilas1.tsx
@@ -5,6 +5,10 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Sekilas1 = () => {
+  const [k, setK] = useState(1);
+
+  const anim = [{class: "absolute bottom-0 flex opacity-0 w-[200vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1] ease-linear"}, {class: "absolute bottom-0 flex w-[200vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1] ease-linear transition-[transform] duration-[20000ms]"}, {class: "absolute top-0 flex opacity-0 w-[200vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1] ease-linear"}, {class: "absolute top-0 flex w-[200vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1] ease-linear transition-[transform] duration-[20000ms]"}];
+  const style = [{transform: "translateX(0)"}, {transform: "translateX(-50%)"}, {transform: "translateX(-50%)"}, {transform: "translateX(0)"}];
   const router = useRouter();
 
   const clickHandler = () => {
@@ -17,6 +21,16 @@ const Sekilas1 = () => {
       once: false,
     });
   }, []);
+
+  const [tm, setTm] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTm(20000);
+      setK((k+1)%2);
+    }, tm);
+    return () => clearInterval(interval);
+  }, [k, tm]);
 
   return (
     <div className="bg-[#0F0B36] flex flex-col items-center justify-center text-white pt-20">
@@ -44,9 +58,26 @@ const Sekilas1 = () => {
         Klaten, tryout beserta pembahasan soal, talkshow, expo kampus, dan
         penampilan hiburan.
       </div>
-      <div className="my-[2vw]">
-        <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1]"></div>
-        <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1]"></div>
+      <div className="my-[2vw] relative">
+        <div className="opacity-0 bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1]"></div>
+        <div className="opacity-0 bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] scale-x-[2] sm:scale-[1]"></div>
+        <div className={anim[k].class} style={style[k]}>
+          <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] sm:scale-[1]"></div>
+          <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] sm:scale-[1]"></div>
+        </div>
+        <div className={anim[(k+1)%2].class} style={style[(k+1)%2]}>
+          <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] sm:scale-[1]"></div>
+          <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] sm:scale-[1]"></div>
+        </div>
+        <div className={anim[k+2].class} style={style[k+2]}>
+          <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] sm:scale-[1]"></div>
+          <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] sm:scale-[1]"></div>
+        </div>
+        <div className={anim[(k+1)%2 + 2].class} style={style[(k+1)%2 + 2]}>
+          <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] sm:scale-[1]"></div>
+          <div className="bg-[url('/beranda/redblastout.png')] w-[100vw] h-[5vw] sm:h-[3vw] bg-contain bg-center bg-no-repeat scale-y-[2] sm:scale-[1]"></div>
+        </div>
+        {/* <div className={anim[(k+1)%2].class} style={style[(k+1)%2]}></div> */}
       </div>
     </div>
   );
